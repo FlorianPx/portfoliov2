8dd445c6c6e7eb4ffafe888ac930a24fd6f964c2
Florian <68642839+FlorianPx@users.noreply.github.com>
2024-12-19 18:55:16 +0100

    feat: add home page

---
+++ src/lib/components/AnimateLogo.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import logo from '@assets/logo.svg';
+  import lines from '@assets/lines.svg';
+  import lightEllipse from '@assets/lightEllipse.svg';
+  import darkEllipse from '@assets/darkEllipse.svg';
+  import gradiantEllipse from '@assets/gradiantEllipse.svg';
+</script>
+
+<div class="w-80 h-84 relative flex items-center justify-center">
+  <img class="absolute z-0" alt="Logo du site" src={lines} />
+  <img
+    class="absolute z-10 bottom-3 right-1"
+    alt="Logo du site"
+    src={darkEllipse}
+  />
+  <img
+    class="absolute z-20 top-2 right-6"
+    alt="Logo du site"
+    src={gradiantEllipse}
+  />
+  <img
+    class="absolute z-30 -left-1 bottom-12"
+    alt="Logo du site"
+    src={lightEllipse}
+  />
+  <img class="absolute z-40" alt="Logo du site" src={logo} />
+</div>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,24 @@
+<script>
+  import AnimateLogo from '../lib/components/AnimateLogo.svelte';
+</script>
+
+<div class="flex items-center space-x-12 my-auto">
+  <div class="max-w-2xl space-y-4">
+    <div class="flex items-center space-x-2">
+      <span class="w-16 h-1 bg-purple"></span>
+      <p class="text-base text-purple font-semibold uppercase">Florian Poux</p>
+    </div>
+    <p class="text-4xl font-normal leading-12 m-0 font-recoleta">
+      Je suis développeur front-end.
+    </p>
+    <div class="flex flex-col text-lg text-light-purple">
+      <p>Bienvenue sur mon portfolio !</p>
+      <p class="text-justify max-w-xl">
+        Vous y découvrirez mes skills, des projets réalisés seul ou en équipe,
+        ainsi que mon CV pour mieux me connaître.
+      </p>
+      <p>Bonne visite !</p>
+    </div>
+  </div>
+  <AnimateLogo />
+</div>
